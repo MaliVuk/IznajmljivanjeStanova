9bb2db08e4c8d7f625f636f675ef0fca69afcb5d
MaliVuk <zltvvu7@gmail.com>
2025-07-15 14:39:24 +0200

    Update NavBarEdunova.jsx

--- frontend/src/components/NavBarEdunova.jsx
+++ frontend/src/components/NavBarEdunova.jsx
@@ -14,7 +14,7 @@ export default function NavBarEdunova() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand className='ruka' onClick={()=>navigate(RouteNames.HOME)}>Edunova APP</Navbar.Brand>
+        <Navbar.Brand className='ruka' onClick={()=>navigate(RouteNames.HOME)}>IznajmljivanjeStanova APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
